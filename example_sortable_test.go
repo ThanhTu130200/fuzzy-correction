551397fd0e7ca29f3bf9f2adddc3838d1b4f93d4
Tu <tuprokr1@gmail.com>
2022-10-07 19:25:10 +0700

    init commit

--- example_sortable_test.go
+++ example_sortable_test.go
@@ -6,6 +6,12 @@ package fuzzy
 import (
 	"fmt"
 	"strings"
+	"testing"
+	"time"
+	"bufio"
+	"os"
+	"io"
+	"bytes"
 )
 
 // Player is a very simple data model.
@@ -21,7 +27,7 @@ func (p *Player) Name() string {
 
 // Team is a collection of Player items. This is where fuzzy.Sortable
 // must be implemented to enable fuzzy sorting.
-type Team []*Player
+type Team []Player
 
 // Default sort.Interface methods
 func (t Team) Len() int      { return len(t) }
@@ -34,72 +40,127 @@ func (t Team) Less(i, j int) bool { return t[i].Name() < t[j].Name() }
 // Comparisons are based on the the full name of the player.
 func (t Team) Keywords(i int) string { return t[i].Name() }
 
+
+func lineCounter(r io.Reader) (int, error) {
+    buf := make([]byte, 32*1024)
+    count := 0
+    lineSep := []byte{'\n'}
+
+    for {
+        c, err := r.Read(buf)
+        count += bytes.Count(buf[:c], lineSep)
+
+        switch {
+        case err == io.EOF:
+            return count, nil
+
+        case err != nil:
+            return count, err
+        }
+    }
+}
+
 // Fuzzy sort players by name.
-func ExampleSort() {
-	var t = Team{
-		&Player{"Alisson", "Becker"},
-		&Player{Firstname: "Adrián"},
-		&Player{"Andy", "Lonergan"},
-		&Player{"Caoimhín", "Kelleher"},
-		&Player{"Virgil", "van Dijk"},
-		&Player{"Joe", "Gomez"},
-		&Player{"Andy", "Robertson"},
-		&Player{"Joel", "Matip"},
-		&Player{"Ki-Jana", "Hoever"},
-		&Player{"Trent", "Alexander-Arnold"},
-		&Player{"Sepp", "van den Berg"},
-		&Player{"Neco", "Williams"},
-		&Player{Firstname: "Fabinho"},
-		&Player{"Georginio", "Wijnaldum"},
-		&Player{"James", "Milner"},
-		&Player{"Naby", "Keita"},
-		&Player{"Jordan", "Henderson"},
-		&Player{"Alex", "Oxlade-Chamberlain"},
-		&Player{"Xherdan", "Shaqiri"},
-		&Player{"Curtis", "Jones"},
-		&Player{"Harvel", "Elliott"},
-		&Player{"Roberto", "Firmino"},
-		&Player{"Sadio", "Mané"},
-		&Player{"Mohamed", "Salah"},
-		&Player{"Takumi", "Minamino"},
-		&Player{"Divock", "Origi"},
-	}
+func TestExampleSort(u *testing.T) {
+	fmt.Println("===================")
+	readFile, err := os.Open("dictionary2.txt")
+
+	count, _ := lineCounter(readFile)
+	t := make(Team, count + 1)
+
+	readFile, err = os.Open("dictionary2.txt")
+
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+ 
+    fileScanner.Split(bufio.ScanLines)
+	
+	i := 0
+    for fileScanner.Scan() {
+        fmt.Println(i)
+		t[i] = Player{Firstname: string(fileScanner.Text())}
+        fmt.Println(fileScanner.Text())
+		i++
+    }
+  
+    readFile.Close()
+	// var t = Team{
+	// 	&Player{Firstname: "nguyễn thị tú"},
+	// 	&Player{Firstname: "hồ_chí_minh"},
+	// 	&Player{Firstname: "nguyễn thanh tú"},
+	// 	&Player{Firstname: "tú mập ú"},
+	// 	&Player{Firstname: "tú xương"},
+	// 	&Player{Firstname: "túng"},
+	// 	&Player{Firstname: "nghèo túng"},
+	// 	&Player{Firstname: "súng"},
+	// 	&Player{Firstname: "mùng"},
+	// 	&Player{Firstname: "mụn"},
+	// 	&Player{Firstname: "cụng"},
+	// 	&Player{Firstname: "hàng tuấn thiên"},
+	// 	&Player{Firstname: "lê thị trang"},
+	// 	&Player{Firstname: "trương văn lanh"},
+	// 	&Player{Firstname: "đoàn minh vương"},
+	// 	&Player{Firstname: "hà nội"},
+	// 	&Player{Firstname: "đà nẵng"},
+	// 	&Player{Firstname: "hải thượng lãn ông"},
+	// 	&Player{Firstname: "đồng nai"},
+	// 	&Player{Firstname: "hò xuân hương"},
+	// 	&Player{Firstname: "hồ tùng mậu"},
+	// }
 	// Unsorted
-	fmt.Println(t[0].Name())
+	// fmt.Println(t[0].Name())
 
 	// Initials
-	Sort(t, "taa")
-	fmt.Println(t[0].Name())
+	// Sort(t, "taa")
+	// fmt.Println(t[0].Name())
 
-	// Initials beat start of string
-	Sort(t, "al")
-	fmt.Println(t[0].Name())
+	// // Initials beat start of string
+	// Sort(t, "al")
+	// fmt.Println(t[0].Name())
 
-	// Start of word
-	Sort(t, "ox")
-	fmt.Println(t[0].Name())
+	// // Start of word
+	// Sort(t, "ox")
+	// fmt.Println(t[0].Name())
 
-	// Earlier in string = better match
-	Sort(t, "x")
-	fmt.Println(t[0].Name())
+	// // Earlier in string = better match
+	// Sort(t, "x")
+	// fmt.Println(t[0].Name())
 
 	// Diacritics ignored if query is ASCII
-	Sort(t, "mane")
+
+	fmt.Println("==========================")
+
+	input := "ho chis minh"
+	start := time.Now()
+	Sort(t, input)
+	fmt.Println("time elapse: ", time.Since(start))
+	// fmt.Println(time.Since(start))
+	fmt.Println("input: " + input)
 	fmt.Println(t[0].Name())
+	fmt.Println(t[1].Name())
+	fmt.Println(t[2].Name())
+	fmt.Println(t[3].Name())
+	fmt.Println(t[4].Name())
+	fmt.Println(t[5].Name())
+	fmt.Println(t[6].Name())
+	fmt.Println(t[7].Name())
+	fmt.Println(t[8].Name())
+	fmt.Println(t[9].Name())
+	fmt.Println(t[10].Name())
+
+	fmt.Println("==========================")
+
+	
+
+	// for i, _ := range t {
+	// 	fmt.Println(t[i].Name())
+	// }
 
 	// But not if query isn't
-	Sort(t, "né")
-	fmt.Println(t[0].Name())
-	Sort(t, "ne")
-	fmt.Println(t[0].Name())
-
-	// Output:
-	// Alisson Becker
-	// Trent Alexander-Arnold
-	// Andy Lonergan
-	// Alex Oxlade-Chamberlain
-	// Xherdan Shaqiri
-	// Sadio Mané
-	// Sadio Mané
-	// Neco Williams
+	// Sort(t, "né")
+	// fmt.Println(t[0].Name())
+	// Sort(t, "ne")
+	// fmt.Println(t[0].Name())
 }
